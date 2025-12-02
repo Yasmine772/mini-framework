ea48f693c2e7b536b8102a7ed6acf8005c8f4c07
yasmin Ebrahim <yasmineebrahim458@gmail.com>
2025-12-02 14:30:46 +0200

    update Response class

--- src/core/Response.php
+++ src/core/Response.php
@@ -1,8 +1,31 @@
 <?php
 
-namespace Center\MiniFramework\core;
+namespace Center\MiniFramework\Core;
 
 class Response
 {
+  private int $statusCode = 200;
+  private array $headers = [];
+    private string $body = '';
+
+  public function setStatusCode(int $statusCode): void
+  {
+      $this->statusCode = $statusCode;
+      http_response_code($statusCode);
+
+  }
+  public function setHeader(string $key,string $value): void
+  {
+      $this->headers[$key] = $value;
+      header("$key: $value");
+  }
+  public function setBody(string $content): void
+  {
+      $this->body =$content ;
+  }
+  public function sendMessage(): void
+  {
+      echo $this->body;
+  }
 
 }
