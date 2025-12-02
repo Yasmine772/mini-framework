7a70e015cd7e61a8d0370f9b59845b37d25261bf
yasmin Ebrahim <yasmineebrahim458@gmail.com>
2025-12-02 12:32:34 +0200

    Add methods path(), method(), query(), body(), headers() to Request.php

--- src/core/Request.php
+++ src/core/Request.php
@@ -1,8 +1,67 @@
 <?php
 
-namespace Center\MiniFramework\core;
+namespace Center\MiniFramework\Core;
 
 class Request
 {
+    /*
+     * Get the HTTP request method in lowercase, default to 'get'
+     *  */
+    public function method()
+    {
+        return strtolower(isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'get');
+    }
+    /*
+     * Return the current Request path without query parameters
+     * */
+    public function path()
+    {
+        $path = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
+        $position = strpos($path,'?');
+        if ($position !== false) {
+            $path = substr($path, 0, $position);
+        }
+        return rtrim($path, '/') ?: '/';
+    }
+    /*
+     * Get all GET request parameters.
+     * */
+    public function query()
+    {
+        return $_GET;
+    }
+    /*
+     * Get all POST  request parameters.
+     * */
+    public function body()
+    {
+        return $_POST;
+    }
+   /*
+   * Get all HTTP request headers.
+   */
+    public function headers()
+    {
+        if (function_exists('getallheaders')) {
+            return getallheaders();
+        }
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (str_starts_with($name, 'HTTP_')) {
+                $headerName = str_replace('_', '-', substr($name, 5));
+                $headers[$headerName] = $value;
+            }
+        }
+        return $headers;
+    }
+
+
+
+
+
+
+
+
+
 
 }
